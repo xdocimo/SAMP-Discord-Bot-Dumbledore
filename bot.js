45aa97cf197c1d93ff777336ed558410e2e0d523
xdocimo <34216821+xdocimo@users.noreply.github.com>
2021-07-08 06:34:55 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -157,7 +157,6 @@ function GetPlayersOnline(msg)
 					fields: [
 						{ name: 'Server IP', value: response['address'], inline: true },
 						{ name: 'En línea', value: response['online'], inline: true },
-						{ name: 'Slots', value: response['maxplayers'], inline: true },
 						{ name: 'Contraseña', value: 'five', inline: true },
 					],
 				}
